2efb6684d6a2f68f88fec8e4850e9a2c5ca8a15a
DWIKI <dwikiadesaputra360@gmail.com>
2026-05-30 23:34:19 +0700

    Add files via upload

---
+++ frontend/src/navigation/AuthNavigator.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import WelcomeScreen from '../screens/WelcomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import AppNavigator from './AppNavigator';
+
+const Stack = createNativeStackNavigator<any, any>();
+
+const AuthNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{
+        headerShown: false,
+        animation: 'fade',
+      }}
+    >
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="MainApp" component={AppNavigator} />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthNavigator;
