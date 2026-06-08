691da291677f122affbf0f579dcca14e72777f0d
Abdul Qoyum <abdulqoyum98@gmail.com>
2026-06-08 09:56:33 +0700

    Update ticketController.js

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -6,7 +6,7 @@ const getAllTickets = async (req, res) => {
         res.status(200).json({ success: true, data: tickets });
     } catch (error) {
         console.error('Kesalahan saat mengambil data tiket:', error);
-        res.status(500).json({ success: false, message: 'Server error' });
+        res.status(500).json({ success: false, message: 'Terjadi kesalahan pada server' });
     }
 };
 
